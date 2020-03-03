7a4f32a7a81c74eb6edbf58fcdcc691afe6bc751
Vitor <ctranimation1@gmail.com>
2020-03-03 10:07:59 -0300

    Criei a interface da página de login.

---
+++ registrar.php
@@ -0,0 +1,30 @@
+
+
+
+
+<html>
+<head>
+	<title>Registrar</title>
+</head>
+<body>
+
+	<div class="box-capsula">
+		<h1>CHAToon</h1>
+		<h3>Entre, convide amigos e falem a vontade!</h3>
+	</div>
+	<div class="login-box login-box-positioned " >
+		<div class="login-box-transparente">
+			<table id="mode-tabela">
+				<tr><td> <input type="text" class="input-username input" placeholder="Seu nome"> </td></tr>
+				<tr><td> <input type="text" class="input-userpass input" placeholder="Sua senha"> </td></tr>
+			</table>
+		</div>
+		<div class="login-box-transparente2">
+			<button class="button-box button-login">Logar</button>
+			<button class="button-box button-register">Registrar</button>
+		</div>
+	</div>
+<link href="styles/register.css" rel="stylesheet" />
+
+</body>
+<html>
